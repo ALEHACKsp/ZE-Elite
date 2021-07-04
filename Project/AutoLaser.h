8cfc7db8f1c281da9857b39f055af329e388ca03
Karaa <64591051+Gamer2068@users.noreply.github.com>
2021-07-04 12:46:55 -0700

    Update AutoLaser.h

--- Project/AutoLaser.h
+++ Project/AutoLaser.h
@@ -7,13 +7,10 @@
 
 class CBaseEntity;
 class CUserCmd;
-class Entities;
 
 namespace AutoLaser
 {
 	void Refresh(CBaseEntity* pLocal, CUserCmd* cmd) noexcept, Run(CBaseEntity* pLocal, CUserCmd* cmd, bool& bSendPacket) noexcept;
 }
-
-void SetReleaseTick(Vector LocalOrigin, Vector EntityOrigin, Vector EntitySpeed, Entities& EntityInfo, CUserCmd* cmd);
 
 #endif
