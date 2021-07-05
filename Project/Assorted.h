dc7a3733b135449e2b0f2e49ac9038991f328031
Karaa <64591051+Gamer2068@users.noreply.github.com>
2021-07-05 15:53:51 -0700

    Update Assorted.h

--- Project/Assorted.h
+++ Project/Assorted.h
@@ -4,27 +4,27 @@
 #include "SDK.h"
 #include "Defintions.h"
 
-extern void Hit_Marker_On_Paint() noexcept;
+void Hit_Marker_On_Paint() noexcept;
 
-extern void SetClanTag(const char* tag) noexcept;
+void SetClanTag(const char* tag) noexcept;
 
-extern void Precache_Custom_Stuff(HMODULE DLL) noexcept;
+void Precache_Custom_Stuff(HMODULE DLL) noexcept;
 
-extern bool PrecacheModel(const char* szModelName) noexcept;
+bool PrecacheModel(const char* szModelName) noexcept;
 
-extern void DrawBeamd(Vector src, Vector end, Color color) noexcept;
+void DrawBeamd(Vector src, Vector end, Color color) noexcept;
 
-extern bool FindStringCS(std::string data, std::string toSearch) noexcept;
+bool FindStringCS(std::string data, std::string toSearch) noexcept;
 
-extern void Set_DisConnection_Msg(const char* Message, bool Reset = false) noexcept;
+void Set_DisConnection_Msg(const char* Message, bool Reset = false) noexcept;
 
-extern std::string ReplaceString(std::string subject, const std::string& search, const std::string& replace) noexcept;
+std::string ReplaceString(std::string subject, const std::string& search, const std::string& replace) noexcept;
 
-extern void UTIL_TraceLine(const Vector& vecAbsStart, const Vector& vecAbsEnd, unsigned int mask, const CBaseEntity* ignore, int collisionGroup, trace_t* ptr) noexcept;
+void UTIL_TraceLine(const Vector& vecAbsStart, const Vector& vecAbsEnd, unsigned int mask, const CBaseEntity* ignore, int collisionGroup, trace_t* ptr) noexcept;
 
 bool IntersectRayWithOBB(const Vector& vecRayStart, const Vector& vecRayDelta, const matrix3x4& matOBBToWorld, const Vector& vecOBBMins, const Vector& vecOBBMaxs) noexcept;
 
-extern void UTIL_ClipTraceToPlayers
+void UTIL_ClipTraceToPlayers
 (
 	const Vector& vecAbsStart,
 	const Vector& vecAbsEnd,
@@ -63,7 +63,7 @@ inline __declspec(naked) void __cdecl Invoke_NET_SetConVar(void* pfn, const char
 	}
 }
 
-inline void NET_Set_ConVar(const char* cvar, const char* value) noexcept
+inline void NET_SetConVar(const char* cvar, const char* value) noexcept
 {
 	static void* pvSetConVar = (void*)(Tools::FindPattern("engine.dll", "56 57 8D 4D D4"));
 
